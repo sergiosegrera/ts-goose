6a01dcda77fef80ac41a877256d80a0a1d71f23a
Sergio Segrera <sergioarturosegrera@gmail.com>
2025-10-27 22:12:41 -0400

    update version command

--- commands/version.ts
+++ commands/version.ts
@@ -1,17 +1,5 @@
-import { readFileSync } from "node:fs";
-import { dirname, join } from "node:path";
-import { fileURLToPath } from "node:url";
+import { version } from "../package.json";
 
 export function versionCommand() {
-  try {
-    // Read package.json to get the version
-    // Go up two levels: commands/ -> root/
-    const currentDir = dirname(fileURLToPath(import.meta.url));
-    const packageJsonPath = join(currentDir, "..", "package.json");
-    const packageJson = JSON.parse(readFileSync(packageJsonPath, "utf-8"));
-    console.log(`ts-goose v${packageJson.version}`);
-  } catch {
-    console.error("Unable to determine version");
-    process.exit(1);
-  }
+  console.log(`ts-goose v${version}`);
 }
